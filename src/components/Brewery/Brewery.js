620bab843dd852a5069307e9fa69ebe5bd71e665
collinkallery <collin.kallery@colorado.edu>
2020-06-04 16:13:39 -0600

    Styling for brewery card

--- src/components/Brewery/Brewery.js
+++ src/components/Brewery/Brewery.js
@@ -1,17 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import {GlobalStyle, darkTheme} from "../../theme/globalStyle";
+import {Route} from "react-router-dom";
+
+const {
+  background,
+  secondaryBackground,
+  textColorGrey,
+  textColorWhite,
+  accent
+} = darkTheme;
+
+const Wrapper = styled.section`
+  width: 80%;
+  border: 1px solid ${secondaryBackground};
+  margin: 2%;
+  padding: 2%;
+
+  h3 {
+    font-size: 1.8em;
+  }
+`
+
+const MapWrapper = styled.section`
+  width: 90%;
+`
 
 const Brewery = (props) => {
 
   return (
-    <section>
-      <h2>{props.name}</h2>
+    <Wrapper>
+      <h3>{props.name}</h3>
       <section>
-        <p>{props.street}</p>
+        <p>Brewery Type: {props.brewery_type}</p>
+        <p>Address: {props.street}</p>
         <p>{props.city}, {props.state}</p>
-        <p>{props.phone}</p>
+        <p>Phone Number: {props.phone}</p>
       </section>
-    </section>
+      <MapWrapper>
+
+      </MapWrapper>
+    </Wrapper>
   )
 }
 
