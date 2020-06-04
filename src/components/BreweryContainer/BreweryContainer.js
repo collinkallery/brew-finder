25edbe8aae0b24fcea052eca1a1580932cbce675
collinkallery <collin.kallery@colorado.edu>
2020-06-04 12:13:47 -0600

    Add in component did mount, map over breweries, setting state

--- src/components/BreweryContainer/BreweryContainer.js
+++ src/components/BreweryContainer/BreweryContainer.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, {Component} from "react";
 import styled from "styled-components";
 import Brewery from '../Brewery/Brewery';
+import {fetchByCity, fetchByState, fetchByZip} from '../../apiCalls';
+import {GlobalStyle, darkTheme} from "../../theme/globalStyle";
+import {Route} from "react-router-dom";
 
-const BreweryContainer = (props) => {
-  const pubsToDisplay = props.pubsByCity.map(pub => {
+class BreweryContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breweries: []
+    }
+  }
+
+  pubsToDisplay = () => {
+    const pubs = this.state.breweries.map(pub => {
+      return (
+        <Brewery {...pub} />
+      )
+    })
+    return pubs;
+  }
+
+  componentDidMount = async () => {
+    if (this.props.searchType === 'City') {
+      await fetchByCity(this.props.searchLocation)
+        .then(data => this.setState({
+          breweries: data
+        }))
+    } else if (this.props.searchType === 'State') {
+      await fetchByState(this.props.searchLocation)
+        .then(data => this.setState({
+          breweries: data
+        }))
+        .then(data => this.pubsToDisplay)
+    } else if (this.props.searchType === 'Zip Code') {
+      await fetchByZip(this.props.searchLocation)
+        .then(data => this.setState({
+          breweries: data
+        }))
+        .then(data => this.pubsToDisplay)
+    }
+  }
+
+  render() {
     return (
-      <Brewery {...pub} />
+      <section>
+        {this.pubsToDisplay()}
+      </section>
     )
-  })
-  return (
-    <section>
-    {pubsToDisplay}
-    </section>
-  )
+  }
 }
 
 export default BreweryContainer;
